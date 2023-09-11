f04961ba1ac881633e53a4d50f8a37ab88718fc8
jackwynne <jack.wynne@outlook.com>
2023-09-11 14:03:43 +1200

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,43 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Power BI',
-					autogenerate: { directory: 'powerbi' },
-				},
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', link: '/guides/example/' },
-				// 	],
-				// },
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Power BI",
+          autogenerate: {
+            directory: "powerbi",
+          },
+        },
+        // {
+        // 	label: 'Guides',
+        // 	items: [
+        // 		// Each item here is one entry in the navigation menu.
+        // 		{ label: 'Example Guide', link: '/guides/example/' },
+        // 	],
+        // },
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
+      ],
+
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
