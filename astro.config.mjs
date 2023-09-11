d60d79170352d3534d04a34594b00785a24884de
jackwynne <jack.wynne@outlook.com>
2023-09-11 13:03:13 +1200

    Basic version of docs using new Starlight template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,34 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import rehypeExternalLinks from "rehype-external-links";
-import remarkCodeTitles from "remark-code-titles";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import sitemap from "@astrojs/sitemap";
-/**
- * @link https://astro.build/config
- */
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 export default defineConfig({
-  site: `https://create.t3.gg`,
-  markdown: {
-    remarkPlugins: [remarkCodeTitles],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-          rel: ["noreferrer noopener"],
-          content: {
-            type: "text",
-            value: "↗",
-          },
-        },
-      ],
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            class: "heading-link heading-link--hidden---effects",
-            "data-heading-link": true,
-          },
-          behavior: "wrap",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "rose-pine",
-      wrap: true,
-    },
-    extendDefaultPlugins: true,
-  },
-  integrations: [
-    react(),
-    tailwind({ config: { applyBaseStyles: false } }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    mdx(),
-  ],
+	integrations: [
+		starlight({
+			title: 'Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Power BI',
+					autogenerate: { directory: 'powerbi' },
+				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', link: '/guides/example/' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
